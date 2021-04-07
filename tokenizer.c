d6587024028be59739c546b05c9afaf22d236a0b
JudgeFudge19 <dremukare@gmail.com>
2021-04-07 14:31:50 +0000

    created tokenizer

--- tokenizer.c
+++ tokenizer.c
@@ -1,29 +1,22 @@
 #include "shell.h"
 
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
 /**
- * main - tokenizes a string
- * Return: 0
- */
-int main(void)
+* tokenizer - tokenizes a string
+* @command: user entered command
+* Return: array of strings
+*/
+char **tokenizer(char *command)
 {
-        char str[] = "This better work bana";
-        char *token ;
-        token  = strtok(str, " ,\n\t\r;");
-        char **tokens = malloc(sizeof(char) * strlen(str + 1));
-        int pos = 0;
+	char *token = strtok(str, " ,\n\t\r;");
+	char **tokens = malloc(sizeof(char) * strlen(str + 1));
+	int pos = 0;
 
-	while(token != NULL)
-        {
-                tokens[pos] = token;
-                printf("tokens[%d] is : %s \n",pos, tokens[pos]);
-                pos++;
-                token = strtok(NULL, " ");
-        }
-
+	while (token != NULL)
+	{
+		tokens[pos] = token;
+		pos++;
+		token = strtok(NULL, " ");
+	}
 	tokens[pos] = NULL;
 	return (0);
 }
-

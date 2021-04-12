4720ae896c00588dc55ae13d280f015f44c93932
JudgeFudge19 <dremukare@gmail.com>
2021-04-12 17:55:48 +0000

    progress so far

--- execution.c
+++ execution.c
@@ -11,21 +11,24 @@ int execution(char **args)
 	pid_t child_pid;
 	int status;
 	char *new_comm = "/bin/";
+	char *envp[] = {"PATH=/bin", NULL};
 
 	child_pid = fork();
 
 	if (child_pid == 0)
 	{
-		if (_strstr("/bin/", args[0]))
+		if (_strstr(args[0], new_comm))
 		{
-			if (execve(args[0], args, NULL) == -1)
+			if (execve(args[0], args, envp) == -1)
 				exit(EXIT_FAILURE);
+			printf("%s\n", args[0]);
 		}
-		else
+		if (!(_strstr(args[0], new_comm)))
 		{
 			_strcat(new_comm, args[0]);
-			if (execve(new_comm, args, NULL) == -1)
+			if (execve(new_comm, args, envp) == -1)
 				exit(EXIT_FAILURE);
+			printf("%s\n", new_comm);
 		}
 	}
 	else
